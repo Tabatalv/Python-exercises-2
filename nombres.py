c4ab754f0d654a39100d42523bd4f193aa060d18
Tabatalv <147752937+Tabatalv@users.noreply.github.com>
2024-05-16 20:48:10 +0200

    Update and rename main8.py to nombres.py

--- nombres.py
+++ nombres.py
@@ -1,6 +1,6 @@
 """Definir una lista con un conjunto de nombres, imprimir la cantidad
 que comienzan con la letra a. También se puede hacer elegir al usuario la letra a buscar.
-(Un poco mas emocionante)"""
+"""
 
 def count_letters(list, letter):
     letter_count = 0
